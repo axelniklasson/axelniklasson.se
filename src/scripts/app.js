dd888d21f4d21a64064be4b6baa301e68d552662
Axel Niklasson <axel.niklasson@live.com>
2017-01-08 23:06:24 +0100

    Show location and date in post listing

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,5 +1,26 @@
 var app = angular.module('axelniklasson', []);
 
+var MONTHS = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
+];
+
+app.filter('prettyDate', function() {
+    return function(dateString) {
+        var date = new Date(dateString);
+        return MONTHS[date.getMonth()] + ' ' + (date.getDay() + 1);
+    };
+});
+
 app.controller('MainController', function($scope, $http) {
     // Get three latest posts from resa.axelniklasson.se
     var req = {
@@ -12,6 +33,7 @@ app.controller('MainController', function($scope, $http) {
 
     $http(req).then(function(response) {
         $scope.posts = response.data;
+        console.log(response.data);
     }, function(err) {
         // Could not fetch posts
     });
