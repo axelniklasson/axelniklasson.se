57a46aee827af8f5065fbc3591a8fe39b914fd85
Axel Niklasson <axel.niklasson@live.com>
2017-07-29 14:32:39 +0200

    Started website overhaul

--- src/scripts/scroll.js
+++ src/scripts/scroll.js
@@ -1,5 +1,5 @@
 // Thanks to Chris Covier on https://css-tricks.com/snippets/jquery/smooth-scrolling/
-
+// Smooth scroll
 $(function() {
   $('a[href*="#"]:not([href="#"])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
@@ -13,4 +13,9 @@ $(function() {
       }
     }
   });
+});
+
+// Close nav on click
+$('.nav a').on('click', function(){
+    $('.navbar-toggle').click() //bootstrap 3.x by Richard
 });
