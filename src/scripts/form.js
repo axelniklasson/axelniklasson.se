06f5f52712d7bc1623455fcff903051d8a49a1ec
Axel Niklasson <axel.niklasson@live.com>
2017-08-25 17:53:01 +0200

    Added form and modified sendmail to use mailman api

---
+++ src/scripts/form.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+
+    var nameInput = $('#form-name');
+    var emailInput = $('#form-email');
+    var messageInput = $('#form-message');
+    var successMessage = $('#success-message');
+    var errorMessage = $('#error-message');
+
+    $('#contact-form').submit(function(event) {
+        $.ajax({
+            type: 'POST',
+            url: '/php/sendmail.php',
+            data: {
+                name: nameInput.val(),
+                email: emailInput.val(),
+                message: messageInput.val()
+            },
+            success: function(data) {
+                nameInput.val('');
+                emailInput.val('');
+                messageInput.val('');
+                successMessage.fadeIn().delay(10000).fadeOut();
+            },
+            error: function(err) {
+                errorMessage.fadeIn().delay(10000).fadeOut();
+            }
+        });
+
+        return false; // Keep page state
+    });
+});
