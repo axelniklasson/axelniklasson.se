f1204a741ea8ac15941846314d8a60b484b5a5cd
Axel Niklasson <axel.niklasson@live.com>
2018-04-20 11:59:18 +0200

    generated boilerplate code

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Profile from '../images/profile.jpg';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+      </div>
+    );
+  }
+}
+
+export default App;
