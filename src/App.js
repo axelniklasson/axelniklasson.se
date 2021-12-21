2920645641a54fd91286a6dbd85661a18620915c
Axel Niklasson <axel.r.niklasson@gmail.com>
2021-12-21 11:41:11 +0100

    Validate that build fails

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import throttle from 'lodash.throttle';
 import React from 'react';
-
 import Divider from './components/Divider';
 import Header from './components/Header';
 import Navbar from './components/Navbar';
